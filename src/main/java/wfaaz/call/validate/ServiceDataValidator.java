2228690ac8ad8da8aaaf994ebbdeba417b8dc155
Opanasenko Oleksii <wfaahz@gmail.com>
2017-03-05 14:32:53 +0200

    Serv data validator skeleton added;

---
+++ src/main/java/wfaaz/call/validate/ServiceDataValidator.java
@@ -0,0 +1,18 @@
+package wfaaz.call.validate;
+
+/**
+ * Created by wfAaz on 05.03.2017.
+ */
+public class ServiceDataValidator {
+    public boolean validateIpAddress(String ip) {
+        throw new UnsupportedOperationException("not implemented yet");
+    }
+
+    public boolean validatePort(int port) {
+        throw new UnsupportedOperationException("not implemented yet");
+    }
+
+    public boolean validateTime(long time) {
+        throw new UnsupportedOperationException("not implemented yet");
+    }
+}
