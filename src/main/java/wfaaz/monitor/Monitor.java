aa2716b4dcf6682d48de4b74094e1e818e9f90f7
Opanasenko Oleksii <wfaahz@gmail.com>
2017-03-05 23:09:06 +0200

    bridge listener for Callers transfer from parser to monitor processing added;

--- src/main/java/wfaaz/monitor/Monitor.java
+++ src/main/java/wfaaz/monitor/Monitor.java
@@ -5,6 +5,6 @@ import java.net.InetSocketAddress;
 /**
  * Created by wfAaz on 05.03.2017.
  */
-public interface Monitor {
+public interface Monitor extends CallerListener {
     void register(InetSocketAddress serviceAddress, long pollTimeSec);
 }
