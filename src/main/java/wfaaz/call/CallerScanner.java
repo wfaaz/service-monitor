7d195071a2f1e9ed58eba0cdd8aa2c621051bc43
Opanasenko Oleksii <wfaahz@gmail.com>
2017-03-06 20:25:24 +0200

    added outage lines read , parce and validate. regexps aded to validator;

--- src/main/java/wfaaz/call/CallerScanner.java
+++ src/main/java/wfaaz/call/CallerScanner.java
@@ -1,8 +1,13 @@
 package wfaaz.call;
 
 import org.apache.log4j.Logger;
+import wfaaz.outage.OutageListener;
 
 import java.net.InetSocketAddress;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
@@ -14,21 +19,31 @@ import static wfaaz.call.validate.ServiceDataValidator.*;
 public class CallerScanner {
     private static Logger log = Logger.getLogger(CallerScanner.class);
 
-    private final static String INPUT_ARGS_FORMAT_MESSAGE = "Input args please. Valid format:\n<ipV4Address>:<port>,<pollTimeOnSeconds>,<graceTimeOnSeconds>";
+    private final static String INPUT_ARGS_FORMAT_MESSAGE = "Input args please. Valid format:\n" +
+            "1) <ipV4Address>:<port>,<pollTimeOnSeconds>,<graceTimeOnSeconds>\n" +
+            "where pollTime and pollTime > 1 sec;\n" +
+            "2) out <ipV4Address>:<port>,<outageStartTime>,<outageEndTime>";
+
     private final static String INVALID_ARGS_FORMAT = "The format is invalid.\n";
     private final static String COMMA = ",";
     private final static String COLON = ":";
+    private final static String OUT_COMMAND = "out";
     private final static long SLEEP_TIMEOUT = 500;
 
+    private SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
     private Scanner scanner;
     private CallerListener callerListener;
+    private OutageListener outageListener;
     private String ip;
     private int port;
     private long pollTimeMs;
     private long graceTimeMs;
+    private Date startOutageTimeMs;
+    private Date endOutageTimeMs;
 
-    public CallerScanner(CallerListener callerListener) {
+    public CallerScanner(CallerListener callerListener, OutageListener outageListener) {
         this.callerListener = callerListener;
+        this.outageListener = outageListener;
         this.scanner = new Scanner(System.in);
     }
 
@@ -57,42 +72,64 @@ public class CallerScanner {
         try {
             throwWrapper(isValidArgSeparators(line));
 
-            String[] addressPollAndGraceParams = parceAddressPollAndGrace(line);
-            String[] addressAndPort = parceAddressAndPort(addressPollAndGraceParams);
+            boolean isOutageParams = line.startsWith(OUT_COMMAND);
+            if (isOutageParams) {
+                line = line.replaceFirst(OUT_COMMAND, "").trim();
+            }
+            parceLineParams(line, isOutageParams);
 
-            parceIpAddress(addressAndPort);
-            parcePort(addressAndPort);
-            parcePollTime(addressPollAndGraceParams);
-            parceGraceTime(addressPollAndGraceParams);
+            InetSocketAddress address = new InetSocketAddress(ip, port);
+            if (!isOutageParams) {
+                Caller caller = new ServiceStatusCaller(address, pollTimeMs, graceTimeMs);
+                callerListener.onNewCaller(caller);
+                log.info("New Caller:" + caller + "added.");
+            } else {
+                log.info("New Outage:" + address + "added, period from " + startOutageTimeMs + " to " + endOutageTimeMs);
+                outageListener.onOutage(address, startOutageTimeMs, endOutageTimeMs);
+            }
 
-            Caller caller = new ServiceStatusCaller(new InetSocketAddress(ip, port), pollTimeMs, graceTimeMs);
-            callerListener.onNewCaller(caller);
-            log.info("New Caller:" + caller + "added.");
         } catch (ServiceDataValidatorException sdve) {
             log.info(INVALID_ARGS_FORMAT + INPUT_ARGS_FORMAT_MESSAGE);
         }
     }
 
-    private String[] parceAddressPollAndGrace(String line) throws ServiceDataValidatorException {
+    private void parceLineParams(String line, boolean isOutageParams) throws ServiceDataValidatorException {
+        String[] addressAndTimeParams = parceAddressAndTiming(line);
+        String[] addressAndPort = parceAddressAndPort(addressAndTimeParams[0]);
+
+        parceIpAddress(addressAndPort);
+        parcePort(addressAndPort);
+        if (!isOutageParams) {
+            parcePollTimeMs(addressAndTimeParams[1]);
+            parceGraceTimeMs(addressAndTimeParams[2]);
+        } else {
+            startOutageTimeMs = parseOutageTiming(addressAndTimeParams[1]);
+            endOutageTimeMs = parseOutageTiming(addressAndTimeParams[2]);
+        }
+    }
+
+    private String[] parceAddressAndTiming(String line) throws ServiceDataValidatorException {
         String[] addressPollAndGrace = line.split(COMMA);
-        throwWrapper(isValidArgNumAddrPollAndGraceTime(addressPollAndGrace));
+        throwWrapper(isValidArgNumAddrAndTiming(addressPollAndGrace));
         return addressPollAndGrace;
     }
 
-    private String[] parceAddressAndPort(String[] addressPollAndGraceParams) throws ServiceDataValidatorException {
-        String[] addressAndPort = addressPollAndGraceParams[0].split(COLON);
+    private String[] parceAddressAndPort(String address) throws ServiceDataValidatorException {
+        String[] addressAndPort = address.split(COLON);
         throwWrapper(isValidArgNumInetAddr(addressAndPort));
         return addressAndPort;
     }
 
-    private void parceGraceTime(String[] addressPollAndGraceParams) throws ServiceDataValidatorException {
-        graceTimeMs = TimeUnit.SECONDS.toMillis(Long.parseLong(addressPollAndGraceParams[2]));
-        throwWrapper(isValidTime(graceTimeMs));
+    private void parceGraceTimeMs(String graceTime) throws ServiceDataValidatorException {
+        throwWrapper(isDigits(graceTime));
+        graceTimeMs = TimeUnit.SECONDS.toMillis(Long.parseLong(graceTime));
+        throwWrapper(isValidTimeMs(graceTimeMs));
     }
 
-    private void parcePollTime(String[] addressPollAndGraceParams) throws ServiceDataValidatorException {
-        pollTimeMs = TimeUnit.SECONDS.toMillis(Long.parseLong(addressPollAndGraceParams[1]));
-        throwWrapper(isValidTime(pollTimeMs));
+    private void parcePollTimeMs(String pollTime) throws ServiceDataValidatorException {
+        throwWrapper(isDigits(pollTime));
+        pollTimeMs = TimeUnit.SECONDS.toMillis(Long.parseLong(pollTime));
+        throwWrapper(isValidTimeMs(pollTimeMs));
     }
 
     private void parcePort(String[] addressAndPort) throws ServiceDataValidatorException {
@@ -103,5 +140,22 @@ public class CallerScanner {
     private void parceIpAddress(String[] addressAndPort) throws ServiceDataValidatorException {
         ip = addressAndPort[0];
         throwWrapper(isValidIpAddress(ip));
+    }
+
+    private Date parseOutageTiming(String outageSdfTime) throws ServiceDataValidatorException {
+        throwWrapper(isValidSimpleDateFormat(outageSdfTime));
+        try {
+            Calendar trueCalendar = Calendar.getInstance();
+            Calendar croppedCalendar = Calendar.getInstance();
+
+            croppedCalendar.setTime(sdf.parse(outageSdfTime));
+            croppedCalendar.add(Calendar.YEAR, trueCalendar.get(Calendar.YEAR) - 1970);
+            croppedCalendar.add(Calendar.DAY_OF_YEAR, trueCalendar.get(Calendar.DAY_OF_YEAR) - 1);
+
+            return croppedCalendar.getTime();
+        } catch (ParseException pe) {
+            log.warn("On parseOutageTiming", pe);
+            throw new ServiceDataValidatorException();
+        }
     }
 }
--- src/main/java/wfaaz/call/validate/ServiceDataValidator.java
+++ src/main/java/wfaaz/call/validate/ServiceDataValidator.java
@@ -9,15 +9,25 @@ import org.apache.log4j.Logger;
  */
 public class ServiceDataValidator {
     private static Logger log = Logger.getLogger(ServiceDataValidator.class);
-    private static Pattern VALID_IPV4_PATTERN = null;
-    private static final String ipv4Pattern = "(([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.){3}([01]?\\d\\d?|2[0-4]\\d|25[0-5])";
+
+    private static final String IPV4_REG_EXP = "(([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.){3}([01]?\\d\\d?|2[0-4]\\d|25[0-5])";
+    private static final String SDF_REG_EXP = "(^([0-1]?\\d|2[0-3]):([0-5]?\\d):([0-5]?\\d)$)|(^([0-5]?\\d):([0-5]?\\d)$)|(^[0-5]?\\d$)";
+
+    private static final String DIGIT_REG_EXP = "[0-9]+";
+    private final static long MIN_TIME_MS = 1000;
+
+    private static Pattern ipV4Pattern;
+    private static Pattern sdfPattern;
+    private static Pattern digitPattern;
     private static int MAX_VALID_PORT = 65535;
     private static String COLON = ":";
     private static String COMMA = ",";
 
     static {
         try {
-            VALID_IPV4_PATTERN = Pattern.compile(ipv4Pattern, Pattern.CASE_INSENSITIVE);
+            ipV4Pattern = Pattern.compile(IPV4_REG_EXP, Pattern.CASE_INSENSITIVE);
+            sdfPattern = Pattern.compile(SDF_REG_EXP, Pattern.CASE_INSENSITIVE);
+            digitPattern = Pattern.compile(DIGIT_REG_EXP, Pattern.CASE_INSENSITIVE);
         } catch (PatternSyntaxException e) {
             log.error("Unable to compile pattern", e);
         }
@@ -34,7 +44,23 @@ public class ServiceDataValidator {
      */
     public static boolean isValidIpAddress(String ipAddress) {
         if (ipAddress != null && !ipAddress.trim().isEmpty()) {
-            return VALID_IPV4_PATTERN.matcher(ipAddress).matches();
+            return ipV4Pattern.matcher(ipAddress).matches();
+        }
+        return false;
+    }
+
+    /**
+     * Determine if the given string is a valid SimpleDateFormat time.  This method
+     * uses pattern matching to see if the given string could be a valid time.
+     *
+     * @param sdfTime A string that is to be examined to verify whether or not
+     *  it could be a valid time on format HH:MM:SS.
+     * @return <code>true</code> if the string is a value that is a valid sdf time,
+     *  <code>false</code> otherwise.
+     */
+    public static boolean isValidSimpleDateFormat(String sdfTime) {
+        if (sdfTime != null && !sdfTime.trim().isEmpty()) {
+            return sdfPattern.matcher(sdfTime).matches();
         }
         return false;
     }
@@ -43,8 +69,15 @@ public class ServiceDataValidator {
         return port >= 0 && port <= MAX_VALID_PORT;
     }
 
-    public static boolean isValidTime(long time) {
-        return time >= 0;
+    public static boolean isDigits(String line) {
+        if (line != null && !line.trim().isEmpty()) {
+            return digitPattern.matcher(line).matches();
+        }
+        return false;
+    }
+
+    public static boolean isValidTimeMs(long time) {
+        return time >= MIN_TIME_MS;
     }
 
 
@@ -59,7 +92,7 @@ public class ServiceDataValidator {
         return isValidNumOfArgs(array, 2);
     }
 
-    public static boolean isValidArgNumAddrPollAndGraceTime(String [] array) {
+    public static boolean isValidArgNumAddrAndTiming(String [] array) {
         return isValidNumOfArgs(array, 3);
     }
 
